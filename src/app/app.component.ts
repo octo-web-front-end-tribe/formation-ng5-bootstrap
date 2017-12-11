a149b48109ae7b16c0cc4c7551cfbb7e361ee3ee
Franck Libam <franck.libam@gmail.com>
2017-12-11 03:54:21 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Framework {
+  logo: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  frameworks: Array<Framework> = [
+    {
+      logo: 'https://angular.io/assets/images/logos/angular/angular.svg',
+      name: 'Angular'
+    },
+    {
+      logo: 'https://docs.microsoft.com/en-us/appcenter/media/logo_react.svg',
+      name: 'ReactJS'
+    },
+    {
+      logo: 'https://vuejs.org/images/logo.png',
+      name: 'VueJS'
+    },
+    {
+      logo: 'https://emberjs.com/images/tomster-sm.png',
+      name: 'EmberJS'
+    }
+  ]
 }
